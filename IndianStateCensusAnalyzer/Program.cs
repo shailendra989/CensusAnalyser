74cccd4f0763b160b2dc4e1ac6bdde935970ac40
Shailendra kumar pal <palshailendrakumar123@gmail.com>
2023-02-20 22:24:56 +0530

    first commit

---
+++ IndianStateCensusAnalyzer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndianStateCensusAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
